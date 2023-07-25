438a1762afa122d78844205f0cbd0c506039a250
Shaun Lindsley <shaunlindsley@gmail.com>
2023-07-25 15:05:23 +0100

    Token cap

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,9 +3,35 @@
 pragma solidity >=0.5.0 <0.9.0;
 pragma abicoder v2;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-	
+contract Token is ERC20PresetMinterPauser, Ownable {
+    uint256 private _cap;
+    bytes32 public constant CAPPER_ROLE = keccak256("CAPPER_ROLE");
+
+	constructor() ERC20PresetMinterPauser("Token", "TKN") {
+        require(20_000_000 > 0, "ERC20Capped: cap is 0");
+        _cap = 20_000_000;
+
+        _setupRole(CAPPER_ROLE, _msgSender());
+
+        _mint(_msgSender(), 1000);
+    }
+
+    function cap() public view virtual returns (uint256) {
+        return _cap; 
+    }
+
+    function changeCap(uint256 _newCap) public virtual {
+        require(hasRole(CAPPER_ROLE, _msgSender()), "Token: must have capper role to set new cap");
+        _cap = _newCap;
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
+
 }
 
--- truffle-config.js
+++ truffle-config.js
@@ -4,17 +4,18 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 5000000
-    }
+      gas: 5000000,
+    },
   },
   compilers: {
     solc: {
+      version: " >=0.5.0 <0.9.0",
       settings: {
         optimizer: {
           enabled: true, // Default: false
-          runs: 200      // Default: 200
+          runs: 200, // Default: 200
         },
-      }
-    }
-  }
+      },
+    },
+  },
 };
